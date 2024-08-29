dc6d523ff2cdbd82861176301e93b91e4831753e
Zenati Mehdi <mehdi1.zenati@epitech.eu>
2024-08-30 00:11:48 +0200

    En fait ils avaient raison settimeout c dla merde mdr

--- src/index.ts
+++ src/index.ts
@@ -22,17 +22,20 @@ controlsSetup(camera, controls);
 
 function animateBullets() {
   bullets.forEach((bullet, index) => {
-
     if (!bullet.userData.direction) return;
-    
+
     scene.add(bullet);
     const direction = bullet.userData.direction as THREE.Vector3;
     bullet.position.add(direction.clone().multiplyScalar(0.1));
-    
-    setTimeout(() => {
+
+    // setTimeout(() => {
+    //   scene.remove(bullet);
+    //   bullets.splice(index, 1);
+    // }, 8000)
+    if (bullet.position.distanceTo(camera.position) > 100) {
       scene.remove(bullet);
       bullets.splice(index, 1);
-    }, 8000)
+    }
   });
 }
 
